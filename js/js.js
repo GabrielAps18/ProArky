76de32dad87d620967cf72b15a2aba11f55963f8
Gabriel <gaps18072001@gmail.com>
2019-11-05 22:16:23 -0400

    .textcolornav
    se creo la class .textcolornav que controla los colores  del nav al hacer scroll

--- js/js.js
+++ js/js.js
@@ -2,18 +2,24 @@ $(document).ready(function() {
 	$(window).scroll(function() {
   	if($(document).scrollTop() > 480) {
     	$('.uk-navbar').css({
-        'background-color': '#303030',
+        'background-color': '#f8f8f8',
         'transition': 'all ease .5s',
-				'color': '#273746',
         'height': '100px'
-        });
+			})
+			$('.textcolornav').css({
+				'color': '#303030'
+			});
     }
     else {
     $('.uk-navbar').css({
       'background-color': 'transparent',
       'transition': 'all ease 0s',
       'height': '80px'
-    });
+    })
+		$('.textcolornav').css({
+			'color': '#f8f8f8'
+		});
+		;
     }
   });
 });
